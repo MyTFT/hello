d63de4d9a1e59616390fc9a8fe9685fd26437846
MyTFT <chrisgintz88@gmail.com>
2026-04-07 12:48:41 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+#Chris Gintz
+#04/07/2026
+#CS50 Journey
+#This program will ask the user for their name and then greet them.
+
+# Ask the user for their name and store in a variable called yourName.
+yourName = input("What's your name? ")
+
+# Outputs a greeting to the user using their name.
+print ("Hello, " + yourName + "!")
